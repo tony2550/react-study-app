89c041f94dc4f7ba2fb0245e31ff14603aeed377
김도현 <mookieum1991@gmail.com>
2022-05-06 15:35:05 +0900

    add Destructuring

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import classes from './Hello.module.css';
-const Hello = (props) => {
+const Hello = ({ name, backnumber, team }) => {
   return (
     <div>
       <section className={classes.Root}>
         <div className={classes.Container}>
           <div>
             <span className={classes.Introduce}>My name is </span>
-            <span className={classes.Username}>{props.name}</span>
+            <span className={classes.Username}>{name}</span>
           </div>
           <div>
             <span className={classes.Introduce}>Back Number </span>
-            <span className={classes.Backnumber}>{props.backnumber}</span>
+            <span className={classes.Backnumber}>{backnumber}</span>
           </div>
           <div>
             <span className={classes.Introduce}>Team Name </span>
-            <span className={classes.Teamname}>{props.team}</span>
+            <span className={classes.Teamname}>{team}</span>
           </div>
         </div>
       </section>
