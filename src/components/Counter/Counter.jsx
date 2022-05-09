797df858201742fd8936d2403fccb77e2afc1165
김도현 <mookieum1991@gmail.com>
2022-05-09 11:24:08 +0900

    add Conecting onClick event and Counter function / add counterNum

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const [number, setNumber] = useState(0);
+
   const onIncrease = () => {
-    console.log('Plus / +');
+    setNumber(number + 1);
   };
 
   const onDecrease = () => {
-    console.log('Minus / -');
+    setNumber(number - 1);
   };
 
   return (
     <div className={classes.Root}>
       <div className={classes.Container}>
         <div className={classes.CounterBoard}>
-          <button className={classes.Btn} onClick={onIncrease}>
+          <button className={classes.Btn} onClick={onDecrease}>
             -
           </button>
 
-          <div className={classes.CounterNum}>0</div>
+          <div className={classes.CounterNum}>{number}</div>
 
-          <button className={classes.Btn} onClick={onDecrease}>
+          <button className={classes.Btn} onClick={onIncrease}>
             +
           </button>
         </div>
