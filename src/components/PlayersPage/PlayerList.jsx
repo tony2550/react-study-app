7721bbee03803a280ef6219368e3758cd7a8ece5
김도현 <mookieum1991@gmail.com>
2022-05-09 16:16:31 +0900

    add Player components in PlayerList Page

--- src/components/PlayersPage/PlayerList.jsx
+++ src/components/PlayersPage/PlayerList.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import classes from './PlayerList.module.css';
 
+const Player = ({ player }) => {
+  return (
+    <div>
+      <h3>NAME : {player.name}</h3>
+      <h3>BackNumber : {player.backnumber}</h3>
+      <h3>Position : {player.position}</h3>
+    </div>
+  );
+};
+
 const PlayerList = () => {
   const players = [
     { id: 1, name: 'Mookie', backnumber: '27', position: 'pitcher' },
@@ -11,21 +21,9 @@ const PlayerList = () => {
     <>
       <div className={classes.Root}>
         <div className={classes.Container}>
-          <div>
-            <h3>NAME : {players[0].name}</h3>
-            <h3>BackNumber : {players[0].backnumber}</h3>
-            <h3>Position : {players[0].position}</h3>
-          </div>
-          <div>
-            <h3>NAME : {players[1].name}</h3>
-            <h3>BackNumber : {players[1].backnumber}</h3>
-            <h3>Position : {players[1].position}</h3>
-          </div>
-          <div>
-            <h3>NAME : {players[2].name}</h3>
-            <h3>BackNumber : {players[2].backnumber}</h3>
-            <h3>Position : {players[2].position}</h3>
-          </div>
+          <Player player={players[0]} />
+          <Player player={players[1]} />
+          <Player player={players[2]} />
         </div>
       </div>
     </>
