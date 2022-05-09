915e52001fad53491f619c4839c8cd887b26e4da
김도현 <mookieum1991@gmail.com>
2022-05-09 11:06:54 +0900

    modify Counter page  Style

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const onIncrease = () => {
+    console.log('Plus / +');
+  };
+
+  const onDecrease = () => {
+    console.log('Minus / -');
+  };
+
   return (
     <div className={classes.Root}>
       <div className={classes.Container}>
         <div className={classes.CounterBoard}>
-          <button className={classes.Btn}>+</button>
+          <button className={classes.Btn} onClick={onIncrease}>
+            -
+          </button>
+
           <div className={classes.CounterNum}>0</div>
-          <button className={classes.Btn}>-</button>
+
+          <button className={classes.Btn} onClick={onDecrease}>
+            +
+          </button>
         </div>
       </div>
     </div>
