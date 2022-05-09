7f60ee799d65e3131c4ba18dd8b9a79a7cd44227
김도현 <mookieum1991@gmail.com>
2022-05-09 14:38:09 +0900

    add onChange with Multiple state / React Immutability

--- src/components/Input/MultipleInputs.jsx
+++ src/components/Input/MultipleInputs.jsx
@@ -2,20 +2,38 @@ import React, { useState } from 'react';
 import classes from './Input.module.css';
 
 const MultipleInputs = () => {
-  const [text, setText] = useState('');
+  const [inputs, setInputs] = useState({
+    name: '',
+    backnumber: '',
+  });
 
-  const onChange = (e) => {};
+  const { name, backnumber } = inputs; // 구조 분해 할당으로 각각의 값 추출
 
-  const onReset = () => {};
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs, // 기존의 input 객체 copy
+      [name]: value, // name 키를 가진 값을 value로 설정하기
+    });
+  };
+
+  const onReset = () => {
+    setInputs({
+      name: '',
+      backnumber: '',
+    });
+  };
 
   return (
     <div className={classes.Container}>
-      <input placeholder="Player Name" />
-      <input placeholder="Back Number" />
+      <input name="name" placeholder="Player Name" onChange={onChange} value={name} />
+      <input name="backnumber" placeholder="Back Number" onChange={onChange} value={backnumber} />
       <button onClick={onReset}>RESET</button>
-      <div>
-        <h1>PLAYER: {text}</h1>
-        name BackNumber
+      <div className={classes.Board}>
+        <h1>PLAYER : </h1>
+        <h1>{name}</h1>
+        <h1>BACK NUMBER : </h1>
+        <h1>{backnumber}</h1>
       </div>
     </div>
   );
