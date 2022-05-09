8f6e2013423c78402b2aee899a9cebc8bc913aa0
김도현 <mookieum1991@gmail.com>
2022-05-09 12:13:15 +0900

    add useState and onChange, onReset function

--- src/components/Input/InputEx.jsx
+++ src/components/Input/InputEx.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Input.module.css';
 
 const InputEx = () => {
+  const [text, setText] = useState('');
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onReset = (e) => {
+    setText('');
+    // 초기화 값으로 set
+  };
+
   return (
     <div className={classes.Root}>
       <div className={classes.Container}>
-        <input />
-        <button>RESET</button>
+        <input onChange={onChange} value={text} />
+        <button onClick={onReset}>RESET</button>
         <div>
-          <h1>값: </h1>
+          <h1>값: {text}</h1>
         </div>
       </div>
     </div>
