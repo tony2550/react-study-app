79a89032b6d0b3124d882d7969e703774c77cb2a
김도현 <mookieum1991@gmail.com>
2022-05-11 15:03:18 +0900

    add using UserDispatch in Player Component

--- src/components/PlayersPage/PlayerList.jsx
+++ src/components/PlayersPage/PlayerList.jsx
@@ -1,22 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useContext } from 'react';
 import classes from './PlayerList.module.css';
+import { UserDispatch } from '../../App'; //import!
 
 const Player = React.memo(({ player }) => {
-  useEffect(() => {
-    console.log(player);
-  }, []);
+  const dispatch = useContext(UserDispatch);
 
   return (
     <div>
       <div>
-        <h3 style={{ cursor: 'pointer', color: player.active ? 'gold' : 'black' }} onClick={() => {}}>
+        <h3
+          style={{ cursor: 'pointer', color: player.active ? 'gold' : 'black' }}
+          onClick={() => {
+            dispatch({ type: 'TOGGLE_PLAYER', id: player.id });
+          }}
+        >
           NAME : {player.name}
         </h3>
         <h3>BackNumber : {player.backnumber}</h3>
         <h3>Position : {player.position}</h3>
       </div>
       <div className={classes.DeleteDiv}>
-        <button onClick={() => {}}>X</button>
+        <button
+          onClick={() => {
+            dispatch({ type: 'REMOVE_PLAYER', id: player.id });
+          }}
+        >
+          X
+        </button>
       </div>
     </div>
   );
