29f42abb9298cbabddc89de4842c9586c94bda65
김도현 <mookieum1991@gmail.com>
2022-05-20 15:07:20 +0900

    add smtp directory , Contact component, install Axios library

--- src/components/CreatePlayer.jsx
+++ src/components/CreatePlayer.jsx
@@ -1,41 +1,56 @@
-import React, { useContext, useCallback, useRef } from 'react';
-import classes from './CreatePlayer.module.css';
-import useInputs from './hooks/useInputs';
-import { UserDispatch } from '../App';
+import React, { useContext, useCallback, useRef } from 'react'
+import classes from './CreatePlayer.module.css'
+import useInputs from './hooks/useInputs'
+import { UserDispatch } from '../App'
 
 const CreatePlayer = () => {
-  const dispatch = useContext(UserDispatch);
-  const nextId = useRef(4);
-  const [{ name, backnumber, position }, onChange, reset] = useInputs({
-    name: '',
-    backnumber: '',
-    position: '',
-  });
+    const dispatch = useContext(UserDispatch)
+    const nextId = useRef(4)
+    const [{ name, backnumber, position }, onChange, reset] = useInputs({
+        name: '',
+        backnumber: '',
+        position: '',
+    })
 
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: 'CREATE_PLAYER',
-      player: {
-        id: nextId.current,
-        name,
-        backnumber,
-        position,
-      },
-    });
-    reset();
-    nextId.current += 1;
-  }, [name, backnumber, position, reset, dispatch]);
+    const onCreate = useCallback(() => {
+        dispatch({
+            type: 'CREATE_PLAYER',
+            player: {
+                id: nextId.current,
+                name,
+                backnumber,
+                position,
+            },
+        })
+        reset()
+        nextId.current += 1
+    }, [name, backnumber, position, reset, dispatch])
 
-  return (
-    <div className={classes.Root}>
-      <div className={classes.Container}>
-        <input name="name" placeholder="Player Name" onChange={onChange} value={name} />
-        <input name="backnumber" placeholder="Back Number" onChange={onChange} value={backnumber} />
-        <input name="position" placeholder="Position" onChange={onChange} value={position} />
-        <button onClick={onCreate}>RESISTER</button>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className={classes.Root}>
+            <div className={classes.Container}>
+                <input
+                    name="name"
+                    placeholder="Player Name"
+                    onChange={onChange}
+                    value={name}
+                />
+                <input
+                    name="backnumber"
+                    placeholder="Back Number"
+                    onChange={onChange}
+                    value={backnumber}
+                />
+                <input
+                    name="position"
+                    placeholder="Position"
+                    onChange={onChange}
+                    value={position}
+                />
+                <button onClick={onCreate}>RESISTER</button>
+            </div>
+        </div>
+    )
+}
 
-export default React.memo(CreatePlayer);
+export default React.memo(CreatePlayer)
