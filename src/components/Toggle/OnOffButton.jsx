fc26faf24f21ebc08b6d5d08659a26aac077cd70
김도현 <mookieum1991@gmail.com>
2022-06-22 18:03:27 +0900

    add togglebutton

---
+++ src/components/Toggle/OnOffButton.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './onoffbutton.css';
+
+const OnOffButton = ({ innername, isOn, handleChange }) => {
+    return (
+        <div className="onoffbtn">
+            <input
+                checked={isOn}
+                onChange={handleChange}
+                type="checkbox"
+                className="onoffbtn-input"
+                name="toggle"
+                id="onoffbtn"
+            />
+            <label htmlFor="onoffbtn" className="onoffbtn-label">
+                {innername}
+            </label>
+        </div>
+    );
+};
+
+export default OnOffButton;
