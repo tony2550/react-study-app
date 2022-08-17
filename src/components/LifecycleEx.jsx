4b837cbdcf2798a986294e3d3f09d6e7e8135684
김도현 <mookieum1991@gmail.com>
2022-08-17 15:00:07 +0900

    add study React Lifecycle

---
+++ src/components/LifecycleEx.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class LifecycleEx extends Component {
+  static getDerivedStateFromProps() {
+    console.log("getDerivedStateFromProps() 호출");
+    return {};
+  }
+  constructor(props) {
+    super(props);
+    this.state = {};
+    console.log("constructor 호출");
+  }
+
+  componentDidMount() {
+    console.log("componmentDidMount() 호출");
+    this.setState({ updated: true });
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate() 호출");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount() 호출");
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log("getSnapshotBeforeUpdate() 호출");
+    return {};
+  }
+
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate() 호출");
+    return {};
+  }
+
+  render() {
+    console.log("render 호출");
+    return null;
+  }
+}
+
+export default LifecycleEx;
